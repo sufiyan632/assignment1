d0f5250657fee95c22ba6200a388e553733b3c52
sufiyan632 <sufiyanp632@gmail.com>
2026-01-05 16:01:59 +0530

    task1 completed

--- task1.py
+++ task1.py
@@ -1,1 +1,15 @@
-print("hello")
+
+
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+
+print("addition",a+b)
+print("subtraction",a-b)
+print("multiplication",a*b)
+print("division",a/b) 
+ 
